9144d9916ada38679b9163444106db8e326359b9
Davi Costa Ruela <108148320+k7MANO@users.noreply.github.com>
2023-09-01 11:10:25 -0300

    Add files via upload

--- trabalho.js
+++ trabalho.js
@@ -1,11 +1,12 @@
 var pesquisa = document.getElementById("caixaDePesquisa")
 var header = document.getElementById("header")
+var Conhecer = document.getElementById("saibaMais")
+var iconeDeBusca = document.getElementById('iconeDePesquisa')
 pesquisa.style.visibility="hidden"
 
-var iconeDeBusca = document.querySelector('#iconeDePesquisa')
 
 //document.querySelector('#iconeDePesquisa').onclick{pesquisa.classList.toggle('active')}
-
+Conhecer.addEventListener("click",abrirPesquisa)
 iconeDeBusca.addEventListener("click",busca)
 window.addEventListener('scroll',descer)
 
@@ -15,6 +16,10 @@ function busca() {
     if(pesquisa.style.visibility == "visible") {
        pesquisa.focus()
     }
+}
+function abrirPesquisa() {
+    pesquisa.classList.toggle('active')
+    pesquisa.style.visibility="visible"
 }
 function descer() {
     if (window.scrollY > 0) {
